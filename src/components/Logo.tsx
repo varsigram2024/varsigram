a845a6f293bd8036b7202f62ba735b0796bc3064
AYB <ayyubiy67@gmail.com>
2025-01-19 19:14:29 +0100

    Added logo and background image to public folder imported logo into files

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,9 @@
-import { GraduationCap } from 'lucide-react';
+import logo from '../public/varsigram logo.png';
 
 export const Logo = () => {
   return (
     <div className="flex items-center gap-2 text-[#750015]">
-      <GraduationCap className="w-8 h-8" />
-      <span className="text-2xl font-bold">VARSIGRAM</span>
+      <img src={logo} alt="VARSIGRAM Logo" className="" />
     </div>
   );
 };
